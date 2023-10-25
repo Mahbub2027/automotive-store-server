7546d3a5755ae1fe83998c31a411b88f78f71dfe
Mahbub2027 <alam201127@gmail.com>
2023-10-25 21:03:20 +0600

    data read from database

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ async function run() {
 
     const productCollection = client.db("productDB").collection("products");
    
+    // read
+    app.get('/products', async(req, res)=>{
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
     // insert
     app.post('/products', async(req, res)=>{
